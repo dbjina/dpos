24220acff07b9f6e0df633c928e9f25ccee9827e
Jin <Jin@DESKTOP-GMDODKS>
2016-10-05 23:00:02 +0900

    Added Supplier DTO

---
+++ src/com/dbjina/pos/bean/Supplier.java
@@ -0,0 +1,47 @@
+package com.dbjina.pos.bean;
+
+public class Supplier {
+	private int sup_seq;
+	private String sup_name;
+	private String sup_contact_number;
+	private String sup_website;
+	private String sup_email;
+	private String sup_memo;
+	
+	public int getSup_seq() {
+		return sup_seq;
+	}
+	public void setSup_seq(int sup_seq) {
+		this.sup_seq = sup_seq;
+	}
+	public String getSup_name() {
+		return sup_name;
+	}
+	public void setSup_name(String sup_name) {
+		this.sup_name = sup_name;
+	}
+	public String getSup_contact_number() {
+		return sup_contact_number;
+	}
+	public void setSup_contact_number(String sup_contact_number) {
+		this.sup_contact_number = sup_contact_number;
+	}
+	public String getSup_website() {
+		return sup_website;
+	}
+	public void setSup_website(String sup_website) {
+		this.sup_website = sup_website;
+	}
+	public String getSup_email() {
+		return sup_email;
+	}
+	public void setSup_email(String sup_email) {
+		this.sup_email = sup_email;
+	}
+	public String getSup_memo() {
+		return sup_memo;
+	}
+	public void setSup_memo(String sup_memo) {
+		this.sup_memo = sup_memo;
+	}
+}

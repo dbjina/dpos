b924f27e1d5669d5d21b28f01ed59e49a1c35a6f
DBJina <dbjina@gmail.com>
2016-10-06 10:31:11 +0900

    added SupplierType.java

---
+++ src/com/dbjina/pos/bean/SupplierType.java
@@ -0,0 +1,19 @@
+package com.dbjina.pos.bean;
+
+public class SupplierType {
+	private int sup_type_seq;
+	private String sup_type;
+	
+	public int getSup_type_seq() {
+		return sup_type_seq;
+	}
+	public void setSup_type_seq(int sup_type_seq) {
+		this.sup_type_seq = sup_type_seq;
+	}
+	public String getSup_type() {
+		return sup_type;
+	}
+	public void setSup_type(String sup_type) {
+		this.sup_type = sup_type;
+	}
+}

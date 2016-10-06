41c46e74121c2ebcbd33fc5d20a80a43719e56ec
DBJina <dbjina@gmail.com>
2016-10-06 10:31:20 +0900

    added SupplierTypeTag.java

---
+++ src/com/dbjina/pos/bean/SupplierTypeTag.java
@@ -0,0 +1,18 @@
+package com.dbjina.pos.bean;
+
+public class SupplierTypeTag {
+	private int sup_seq;
+	private int sup_type_seq;
+	public int getSup_seq() {
+		return sup_seq;
+	}
+	public void setSup_seq(int sup_seq) {
+		this.sup_seq = sup_seq;
+	}
+	public int getSup_type_seq() {
+		return sup_type_seq;
+	}
+	public void setSup_type_seq(int sup_type_seq) {
+		this.sup_type_seq = sup_type_seq;
+	}
+}

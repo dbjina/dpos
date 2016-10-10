44b532397839fb10457dd1d887625d8ec618e442
DBJina <dbjina@gmail.com>
2016-10-10 09:47:32 +0900

    modified custom.js

--- WebContent/js/custom.js
+++ WebContent/js/custom.js
@@ -3,6 +3,12 @@
  * Copyright since 2016
  * Licensed under the MIT license
  */
+
+/*
+ * Make highlight when a table is on mouse up
+ * @param table a table object
+ * @param cls a class name that will be attached to each of tr of the table object
+ */
 function makeTableHightlightByClick(table, cls) {
 	$(function() {
 		$(table).find("tbody").find("tr").mouseup(function() {
